4a51a73deb2a1209eb0e29ff831d8bb6de19178d
Raghuveer Nallari <raghuveer_nallari@tecnics.com>
2024-03-07 10:34:51 +0000

    First Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["drlpoc/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"drlpoc/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
